e29fab72d636d564293c3e79650b6500381680e3
mikhannik <romanov5867@mail.ru>
2022-04-23 15:38:51 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main () {
+    /*
+    * как
+    * тебя
+    * зовут */
+    println ("Hello, my name is Mikhail")
+    /* сколько тебе лет */
+    println ("I am 23 years old")
+    /* имеешь ли ты какой-то опыт в IT */
+    print ("I graduated from college with a degree in System Administrator, then graduated from the institute " +
+            "with a degree in computer science and computer engineering. I am currently working as a system administrator")
+        //print ("Kotlin")
+}
